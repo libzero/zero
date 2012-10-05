609015b6fc7e5d7017ee47f7655137a3da7cb100
Gibheer <gibheer@gmail.com>
2012-10-05 08:12:34 +0200

    small changes to get it working

--- lib/zero/response.rb
+++ lib/zero/response.rb
@@ -4,7 +4,7 @@ module Zero
   # The response for rack has to be an array of three elements. This class makes
   # it easier to fill all the needed data.
   class Response
-    attr_reader :code, :header, :body
+    attr_accessor :code, :header, :body
 
     # init a new response
     def initialize
@@ -16,7 +16,7 @@ module Zero
     # builds the response for rack and checks for protocol errors
     # @return Array the rack response
     def response
-
+      [@code, @header, @body]
     end
   end
 end
