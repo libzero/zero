92e199d5db76c5fdebf58af53fc7557b42c06b3e
Gibheer <gibheer@gmail.com>
2012-10-06 20:18:30 +0200

    Rack::Response and Rack::Request work pretty good

--- lib/zero/controller.rb
+++ lib/zero/controller.rb
@@ -1,29 +1,26 @@
 module Zero
-
   # abstract class to make creation of controllers easier
   #
   # This abstract class creates an interface to make it easier to write
   # rack compatible controllers. It catches #call and creates a new instance
   # with the environment and calls #render on it.
   class Controller
+    # initialize a new instance of the controller and call response on it
     def self.call(env)
-      new(Request.new(env)).response
+      new(Rack::Request.new(env)).response
     end
 
+    # initialize the controller
+    # @param Request a request object
     def initialize(request)
       @request  = request
-      @code     = 200
-      @header   = {}
-      @body     = ''
+      @response = Rack::Response.new
     end
 
+    # build the response and return it
+    # @return Response a rack conform response
     def response
-      render
-      [@code, @header, [@body]]
-    end
-
-    def render
-      raise NotImplementedError.new
+      raise NotImplementedError.new("Not Implemented in ${__FILE__}")
     end
   end
 end
