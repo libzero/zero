51d2612dc50d82603025717058d17c7c9cbe9445
Gibheer <gibheer@gmail.com>
2012-11-14 20:11:54 +0100

    added specs for #query

---
+++ spec/unit/request/parameter/query_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Zero::Request::Parameter, '#query' do
+  subject { Zero::Request::Parameter.new(env) }
+
+  context "without parameters" do
+    let(:env) { EnvGenerator.get('/foo') }
+    its(:query) { should == {} }
+  end
+
+  context "with a query string" do
+    let(:env) { EnvGenerator.get('/foo?bar=baz') }
+    its(:query) { should == {'bar' => 'baz'} }
+  end
+
+  context "with a post body" do
+    let(:env) { EnvGenerator.post('/foo', {:input => 'bar=baz'}) }
+    its(:query) { should == {} }
+  end
+end
