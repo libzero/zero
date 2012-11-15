35150a7c090259ef1737be96f4ec38a6cd178969
Gibheer <gibheer@gmail.com>
2012-11-15 17:25:12 +0100

    check for an instance of parameter

--- spec/unit/request/params_spec.rb
+++ spec/unit/request/params_spec.rb
@@ -4,11 +4,5 @@ describe Zero::Request, '#params' do
   subject { Zero::Request.new(env) }
   let(:env) { EnvGenerator.get('/foo?bar=baz') }
 
-  it "returns a Parameter object" do
-    subject.params.class.should be(Zero::Request::Parameter)
-  end
-
-  it "returns a valid Parameter object" do
-    subject.params.query['bar'].should == 'baz'
-  end
+  its(:params) { should be_an_instance_of(Zero::Request::Parameter) }
 end
