2f57c9965e5a748639f52b4ee24b91c860ec2543
Gibheer <gibheer@gmail.com>
2012-11-16 17:56:25 +0100

    not needed anymore

--- spec/unit/request/media_types_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Zero::Request, '#media_types' do
-  subject { Zero::Request.new(env) }
-  let(:env) { EnvGenerator.get('/foo') }
-  its(:media_types) { should be_an_instance_of(Zero::Request::Accept) }
-end
