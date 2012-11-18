2e073a07fac3978b28ef348c4c73d17ca68d0722
Stormwind <stormwind@stormwinds-page.de>
2012-11-18 16:38:38 +0100

    Restructure response specs
    Also no change in behaviour

--- spec/unit/response/response_spec.rb
+++ spec/unit/response/response_spec.rb
@@ -1,44 +1,47 @@
 require 'spec_helper'
 
-describe Zero::Response, '#finish' do
+describe Zero::Response do
   subject { Zero::Response.new() }
-  
-  it "returns an array within status header and body" do
-    subject.status = 200
-    subject.header = {}
-    subject.body   = []
 
-    value = subject.to_a
+  describe '#finish' do
+    it "returns an array within status header and body" do
+      subject.status = 200
+      subject.header = {}
+      subject.body   = []
 
-    value.should be_an_instance_of(Array)
-    value[0].should eq(200) # Status code
-    value[1].should eq({})  # Headers
-    value[2].should eq([])  # Body
-  end
-end
+      value = subject.to_a
 
-describe Zero::Response, '#status' do
-  it "must return the status always as an integer" do
-    subject.status = "foobar"
-    subject.status.should eq(0)
-
-    subject.status = 240.5
-    subject.status.should eq(240)
+      value.should be_an_instance_of(Array)
+      value[0].should eq(200) # Status code
+      value[1].should eq({})  # Headers
+      value[2].should eq([])  # Body
+    end
   end
 
-  it "must return 200, if no status code was set" do
-    subject.status.should eq(200)
-  end
-end
+  describe '#status' do
+    it "must return the status always as an integer" do
+      subject.status = "foobar"
+      subject.status.should eq(0)
 
-describe Zero::Response, '#header' do
-  it "must return an empty hash, if no header was set" do
-    subject.header.should eq({})
-  end
-end
+      subject.status = 240.5
+      subject.status.should eq(240)
+    end
 
-describe Zero::Response, '#body' do
-  it "must return an empty array, if no body was set" do
-    subject.body.should eq([])
+    it "must return 200, if no status code was set" do
+      subject.status.should eq(200)
+    end
   end
+
+  describe '#header' do
+    it "must return an empty hash, if no header was set" do
+      subject.header.should eq({})
+    end
+  end
+
+  describe '#body' do
+    it "must return an empty array, if no body was set" do
+      subject.body.should eq([])
+    end
+  end
+
 end
