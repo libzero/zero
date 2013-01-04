7870c5c51fdaa438c2de1f175a7506afdb3a6280
Stormwind <stormwind@stormwinds-page.de>
2013-01-04 19:42:25 +0100

    Kill mutant in Zero::Request::Server

--- spec/unit/request/server/port_spec.rb
+++ spec/unit/request/server/port_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 
 describe Zero::Request::Server, '#port' do
   subject { Zero::Request::Server.new(env) }
-  let(:env) { EnvGenerator.get('/foo', {'SERVER_PORT' => 80}) }
-  its(:port) { should be(80) }
+
+  context 'sets the port to the given value' do
+    let(:env) { EnvGenerator.get('/foo', {'SERVER_PORT' => 80}) }
+    its(:port) { should be(80) }
+  end
+
+  context 'casts also the port to an integer, while setting it' do
+    let(:env) { EnvGenerator.get('/foo', {'SERVER_PORT' => '80'}) }
+    its(:port) { should be(80) }
+  end
 end
