c75f61de6e73ced2be713f225d165b5abb2b1c1a
Gibheer <gibheer@gmail.com>
2013-01-11 08:06:35 +0100

    added a spec for custom parameters

--- spec/unit/zero/request/parameter/initialize_spec.rb
+++ spec/unit/zero/request/parameter/initialize_spec.rb
@@ -6,6 +6,10 @@ describe Zero::Request::Parameter, '#initialize' do
     'zero.params.custom' => {'foo' => 'bar'}
   }) }
 
+  it 'reads the custom parameters' do
+    expect(subject['foo']).to eq('bar')
+  end
+
   it 'does not overwrite parameters' do
     subject
     expect(env['zero.params.custom']).to have_key('foo')
